6e07f36ac67af570587366730d708bf5e374208b
astsidvan <astsidvan@gmail.com>
2022-08-16 17:41:54 +0300

    first commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,5 +1,17 @@
-packege ru.netology.sqr;
+package ru.netology.sqr;
 
 public class SQRService {
 
+    public int calculate(int min, int max) {
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+            int sqr = i * i;
+            if (sqr >= min) {
+                if (sqr <= max) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
